0a7c18fe452c7e6837b865fee3492f49aac7801f
Sumukh M G <msumukh@velocityclinical.com>
2023-10-31 16:32:40 +0530

    day1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@
 function App() {
   return (
     <div>
+    {/* <div>
       Phone Directory
+    </div>
+    <button>Add</button>
+    <div>
+      <span>Name</span>
+      <br/>
+      <span>Phone</span>
+    </div> */}
+    <label htmlFor="name">Name: </label>
+    <input id="name" type="text" placeholder="Type here" defaultValue="Sumukh"/>
     </div>
   );
 }
